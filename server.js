93c34e1416326e0f0beb2d7846614f6322567d05
Siddiqui-2583 <nurnaby83@gmail.com>
2020-12-18 12:22:24 +0600

    DB connected

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
+const mongoose=require('mongoose')
+
+const dbUser = 'dbUser'
+const pass = 'Yrm1sdrmp9GZMOLK'
+const uri = `mongodb+srv://${dbUser}:${pass}@cluster0.evhow.mongodb.net/materialisedView?retryWrites=true&w=majority`;
 
 
 const setMiddleware =require('./api/Middlewares/Middleware');
@@ -14,13 +19,23 @@ setMiddleware(app);
 //Using Routes from Routes directory 
 setRoutes(app);
 
+// var PORT= process.env.PORT || 5000;
+// const server = http.createServer(app);
+
+// server.listen(PORT,()=>{
+//       console.log("Server listening on PORT: "+PORT);
+// });
 
 
+const PORT = process.env.PORT || 5000
 
-
-var PORT= process.env.PORT || 5000;
-const server = http.createServer(app);
-
-server.listen(PORT,()=>{
-      console.log("Server listening on PORT: "+PORT);
-});
+mongoose
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`SERVER IS RUNNING ON PORT ${PORT}`)
+        })
+    })
+    .catch((e) => {
+        console.log(e)
+    })
