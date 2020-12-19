33583109acc1361108652745e7c7f2c0b8460454
AfsanaBrishty <afsanarain603@gmail.com>
2020-12-19 21:55:06 +0600

    query for insertion

--- api/model/postgresql_connection.js
+++ api/model/postgresql_connection.js
@@ -1,16 +1,17 @@
 const { Pool, Client } = require('pg')
 
 const connectionString='postgres://ueuussfr:KKHc_vSfnNsM_qsKkjla3BUxaFQeQs0l@kandula.db.elephantsql.com:5432/ueuussfr';
-const pool = new Pool({
+const pool = new Pool
+({
   connectionString,
 })
 
 module.exports = pool;
 
-pool.query('Select * From contact', function(err, result, fields) {
+pool.query("INSERT INTO contact (uid,username,email,subject,message,createdat) VALUES (8,'H','h@gmail.com','QGTFR','Not Bad','2020-12-19')", function(err,result, fields) {
     if(err) {
       return console.error('error running query', err);
     }
-    return console.log(result);
+    return console.log("1 record inserted");
     pool.end();
   });
