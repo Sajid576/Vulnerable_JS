a95ca8531d0b5440848f2b8adac5995c9433c33c
Sajid576 <sajidahmed696@gmail.com>
2021-02-28 12:54:51 +0600

    security folder added

---
+++ api/Security/NO_SQLI.js
@@ -0,0 +1,28 @@
+
+
+app.post('/posts/:id',(req, res) => {
+    const query = {type: req.body.desiredType};
+
+    Document.find(query).exec()
+       .then((r) => {
+           res.json(r);
+       })
+
+
+});
+
+
+/**
+ * req.body= {desiredType:"blog"}
+ * query   = {type:"blog"}
+ * outcome = All documents which 'type' equals 'blog'
+ * 
+ */
+
+
+ /**
+ * req.body= {desiredType:{$ne: 0 }   }
+ * query   = {type:{$ne: 0 }}
+ * outcome = All documents which field 'type' does not equal 0 
+ * 
+ */
