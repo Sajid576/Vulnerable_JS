3b834bb73eab06ebeb7afb8a195a06c4dcc4b29e
Siddiqui-2583 <nurnaby83@gmail.com>
2021-06-27 17:39:42 +0600

    sql nosql routes and controllers separated, 1 nosql attack is done!

---
+++ api/controller/user_controller_Sql.js
@@ -0,0 +1,106 @@
+const User = require('../model/user');
+const csv_model = require('../model/csv_model');
+const utilities = require('../utility/utilities');
+const pool = require('../Config/postgresql_connection')
+
+exports.signup = (req, res) => {
+    const id = Math.floor(Math.random() * 1000);
+    console.log("inserting data into Postgresql");
+    const query = "'" + id + "','" + username + "','" + password + "'";
+    console.log(query);
+    pool.query(
+      "INSERT INTO Product (productId,title,unitPrice) VALUES (" + query + ")",
+      function (err, result, fields) {
+        if (err) {
+          console.log("error running query", err);
+          res.json(err.message);
+        } else {
+          res.status(200).json(result["rows"]);
+        }
+      }
+    );
+}
+    
+
+
+exports.login = (req, res) => {
+  // select * from Users where username='sajid' and password='123456' ;
+  const query="select * from Users where username='"+req.body.username+"' and password='"+req.body.password+"'"
+  console.log(query);
+  pool.query(
+    query,
+    function (err, result, fields) {
+      if (err) {
+        console.log("error running query", err);
+        res.json(err.message);
+      } else {
+        
+        csv_model.query = query;
+        csv_model.response = JSON.stringify(result["rows"]);
+        utilities.csvStore(csv_model);
+        res.status(200).json(result["rows"]);
+      }
+    }
+  );
+
+
+  
+};
+
+exports.getUserByLength = (req, res) => {
+ 
+  const query= "User.find({ username:"+ req.body.username+" });";
+  User.find({where: function(){
+            return this.age == 23
+              
+  } })
+    .then((response) => {
+      
+      csv_model.query=query;
+      csv_model.response=JSON.stringify(response);;
+      //utilities.csvStore(csv_model);
+      res.json(response);
+    })
+    .catch((e) => {
+      console.log(e);
+      res.json({
+        message: "Error Occurred",
+      });
+    });
+};
+
+exports.deleteUser = (req, res) => {
+  // console.log("hlw")
+let { id } = req.body.id;
+const query="User.findOneAndDelete({_id:"+ id+"})";
+
+User.findOneAndDelete({_id: id,})
+  .then((response) => {
+    csv_model.query=query;
+    csv_model.response=JSON.stringify(response);;
+    utilities.csvStore(csv_model);
+    res.json(response);
+  })
+  .catch((e) => {
+    console.log(e);
+    res.json({
+      message: "Error Occurred",
+    });
+  });
+};
+
+exports.getAllUsers = (req, res) => {
+    User.find()
+        .then(response => {
+            res.json(response)
+            // addExcelData(res)
+        })
+        .catch(e => {
+            console.log(e)
+            res.json({
+                message: 'Error Occurred'
+            })
+        })
+}
+
+
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,18 +1,23 @@
-const user_route = require('./user_route');
-const routes=[
-    {
-        path:'/userapi', 
-        handler: user_route
+const user_route_Sql = require('./user_route_Sql');
+const user_route_NoSQL = require("./user_route_NoSQL");
+const routes = [
+  {
+    path: "/sql/userapi",
+    handler: user_route_Sql,
+  },
+  {
+    path: "/NoSQL/userapi",
+    handler: user_route_NoSQL,
+  },
+  {
+    path: "/",
+    handler: () => {
+      res.json({
+        message: "Hello Developer!!!!!!!!!",
+      });
     },
-    {
-        path: '/',
-        handler:(req,res)=>{
-            res.json({
-                message: "Hello Developer!!!!!!!!!"
-            })
-        }
-    }
-]
+  },
+];
 
 module.exports = (app) => {
         routes.forEach(r =>{
